a9279625996e2ad93ee5502a5f3150d73ccf72c9
tokyoamado <limgcamo@gmail.com>
2022-03-21 12:07:37 +0900

    update scene and script

--- Assets/Scenes/LmmakerScript.cs
+++ Assets/Scenes/LmmakerScript.cs
@@ -5,24 +5,70 @@ using toio;
 
 public class LmmakerScript : MonoBehaviour
 {
+    const int numCubes = 2;
     public ConnectType connectType;
 
+    int mode = 0;  // 0:stop, 1:record, 2:play
+    int[] phase = new int[numCubes];
+    List<Vector2>[] pos = new List<Vector2>[numCubes];
+
+    float intervalTime = 0.1f;
+    float elapsedTime = 0.0f;
+
     CubeManager cm;
+
     async void Start()
     {
-        // ConnectType.Auto - ビルド対象に応じて内部実装が自動的に変わる
-        // ConnectType.Simulator - ビルド対象に関わらずシミュレータのキューブで動作する
-        // ConnectType.Real - ビルド対象に関わらずリアル(現実)のキューブで動作する
         cm = new CubeManager(connectType);
-        await cm.MultiConnect(2);
+        await cm.MultiConnect(numCubes);
+        foreach(var handle in cm.handles)
+        {
+            handle.borderRect = new RectInt(34, 34, 949, 898);
+        }
+        for(int i = 0; i < numCubes; i++)
+        {
+            pos[i] = new List<Vector2>();
+            phase[i] = 0;
+        }
     }
-
 
     void Update()
     {
-        foreach(var cube in cm.syncCubes)
+        elapsedTime += Time.deltaTime;
+        if(elapsedTime >= intervalTime && cm.synced)
         {
-            cube.Move(50, -50, 100);
+            elapsedTime = 0.0f;
+            for(int i = 0; i < cm.handles.Count; i++)
+            {
+                switch(mode)
+                {
+                    case 0:  // stop
+                        phase[i] = 0;
+                        if(pos[i].Count > 0)
+                            cm.handles[i].Move2Target(pos[i][0]).Exec();
+                        break;
+                    case 1:  // record
+                        pos[i].Add(cm.handles[i].cube.pos);
+                        break;
+                    case 2:  // play
+                        if(phase[i] < pos[i].Count - 1)
+                        {
+                            phase[i]++;
+                            cm.handles[i].Move2Target(pos[i][phase[i]]).Exec();
+                        }
+                        break;
+                }
+            }
         }
+    }
+
+    public void Rec() { mode = 1; }
+    public void Play() { mode = 2; }
+    public void Stop() { mode = 0; }
+    public void Clear()
+    {
+        mode = 0;
+        for(int i = 0; i < numCubes; i++)
+            pos[i].Clear();
     }
 }
